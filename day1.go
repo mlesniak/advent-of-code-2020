a280b52f6625f055a0ae99b7eb04bbe1ac4a09d4
Michael Lesniak <mail@mlesniak.com>
2020-12-01 06:15:24 +0100

    Refactoring

--- day1.go
+++ day1.go
@@ -1,28 +1,7 @@
 package main
 
-import (
-	"io/ioutil"
-	"strconv"
-	"strings"
-)
-
 func main() {
-	bs, err := ioutil.ReadFile("input-1.txt")
-	if err != nil {
-		panic(err)
-	}
-	lines := strings.Split(string(bs), "\n")
-	var numbers []int
-	for _, line := range lines {
-		if line == "" {
-			continue
-		}
-		n, err := strconv.Atoi(line)
-		if err != nil {
-			panic(err)
-		}
-		numbers = append(numbers, n)
-	}
+	numbers := readNumbers("input-1.txt")
 
 	for _, n1 := range numbers {
 		for _, n2 := range numbers {
---
+++ util.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func readNumbers(filename string) []int {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(string(bs), "\n")
+	var numbers []int
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		n, err := strconv.Atoi(line)
+		if err != nil {
+			panic(err)
+		}
+		numbers = append(numbers, n)
+	}
+	return numbers
+}
