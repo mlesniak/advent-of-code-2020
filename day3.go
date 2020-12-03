5c568630b3c779894223a062d06edab0e6be1fcb
Michael Lesniak <mail@mlesniak.com>
2020-12-03 06:21:41 +0100

    Day 3 / Part 2

--- day3.go
+++ day3.go
@@ -4,23 +4,41 @@ import "fmt"
 
 func day3() {
 	grid := read2D("input/3.txt")
-	trees := 0
 
-	// Slope
-	dx := 3
-	dy := 1
+	// Slopes
+	slopes := []struct {
+		dx int
+		dy int
+	}{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
+	}
 
+	product := 1
+	for _, slope := range slopes {
+		trees := computeSlope(grid, slope.dx, slope.dy)
+		product *= trees
+	}
+	fmt.Printf("%v\n", product)
+}
+
+func computeSlope(grid Grid, dx int, dy int) int {
 	x := 0
 	y := 0
+	count := 0
+
 	for y < grid.Height {
 		objectAt := grid.Data[y][x%grid.Width]
 		if objectAt == '#' {
-			trees++
+			count++
 		}
 
 		x += dx
 		y += dy
 	}
 
-	fmt.Printf("Trees: %v\n", trees)
+	return count
 }
