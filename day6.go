5450dea52e67f23df86405df5caf7dc27a93a7d3
Michael Lesniak <mail@mlesniak.com>
2020-12-06 08:14:46 +0100

    Day 6 / Part 2

--- day6.go
+++ day6.go
@@ -1,15 +1,36 @@
 package main
 
 // Count number of yes votes for each character
-type Group = map[byte]int
+type Group struct {
+	Size   int
+	Counts map[byte]int
+}
 
 func day6() {
 	glines := readGroupedLines("input/6.txt")
-	groups := parseGroup(glines)
+	counts := parseGroup(glines)
 
+	day6part1(counts)
+	day6part2(counts)
+}
+
+func day6part2(groups []Group) {
 	sum := 0
 	for _, group := range groups {
-		sum += len(group)
+		for _, v := range group.Counts {
+			if v == group.Size {
+				sum++
+			}
+		}
+	}
+
+	println(sum)
+}
+
+func day6part1(groups []Group) {
+	sum := 0
+	for _, group := range groups {
+		sum += len(group.Counts)
 	}
 	println(sum)
 }
@@ -18,15 +39,18 @@ func parseGroup(groupedLine [][]string) []Group {
 	var groups []Group
 
 	for _, group := range groupedLine {
-		g := make(Group)
+		counts := make(map[byte]int)
 		for _, member := range group {
 			for i := range member {
 				vote := member[i]
-				g[vote] = g[vote] + 1
+				counts[vote] = counts[vote] + 1
 			}
 		}
 
-		groups = append(groups, g)
+		groups = append(groups, Group{
+			Size:   len(group),
+			Counts: counts,
+		})
 	}
 
 	return groups
