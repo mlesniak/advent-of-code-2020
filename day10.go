99abb251e87d4a1f05fa80f54e11403cfe0d1103
Michael Lesniak <mail@mlesniak.com>
2020-12-10 19:21:18 +0100

    Day 10 / Part 2

--- day10.go
+++ day10.go
@@ -3,11 +3,33 @@ package main
 import (
 	"fmt"
 	"math"
+	"sort"
 )
 
 func day10() {
 	adapters := readNumbers("input/10.txt")
 
+	//d10part1(adapters)
+	d10part2(adapters)
+}
+
+func d10part2(adapters []int) {
+	sort.Ints(adapters)
+	counter := make(map[int]int) // Cool property of maps: for non-existing values we have 0
+
+	adapters = append(adapters, adapters[len(adapters)-1]+3)
+	counter[0] = 1
+
+	// Walk through the array and see how many ways we had to get to the particular jolt
+	// using memoization. This is way easier than doing combinatorics.
+	for _, jolt := range adapters {
+		counter[jolt] = counter[jolt-1] + counter[jolt-2] + counter[jolt-3]
+	}
+
+	println(counter[adapters[len(adapters)-1]])
+}
+
+func d10part1(adapters []int) {
 	currentJolt := 0
 	jolt1 := 0
 	jolt3 := 0
