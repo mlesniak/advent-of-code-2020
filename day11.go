7fcbbb7dd485ee5bcce684fa309df8ad5b24c688
Michael Lesniak <mail@mlesniak.com>
2020-12-11 06:47:54 +0100

    Day 11 / Part 2 / Testcase

--- day11.go
+++ day11.go
@@ -41,14 +41,15 @@ func computeCycle(grid [][]byte) ([][]byte, int) {
 				continue
 			}
 
-			occupiedSeats := computeOccupiedNeighbourSeats(grid, y, x)
+			//occupiedSeats := computeOccupiedNeighbourSeats(grid, y, x)
+			occupiedSeats := computeVisibleOccupiedSeats(grid, y, x)
 			if seat == 'L' && occupiedSeats == 0 {
 				tmp[y][x] = '#'
 				numChanges++
 				continue
 			}
 
-			if seat == '#' && occupiedSeats >= 4 {
+			if seat == '#' && occupiedSeats >= 5 {
 				tmp[y][x] = 'L'
 				numChanges++
 				continue
@@ -59,6 +60,51 @@ func computeCycle(grid [][]byte) ([][]byte, int) {
 	}
 
 	return tmp, numChanges
+}
+
+func computeVisibleOccupiedSeats(grid [][]byte, y int, x int) int {
+	dxs := []int{-1, 0, +1}
+	dys := []int{-1, 0, +1}
+
+	count := 0
+	for _, dx := range dxs {
+		for _, dy := range dys {
+			if dx == 0 && dy == 0 {
+				continue
+			}
+			if isSeatOccupiedInLineOfSight(grid, y, x, dy, dx) {
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
+func isSeatOccupiedInLineOfSight(grid [][]byte, y int, x int, dy int, dx int) bool {
+	width := len(grid[0])
+	height := len(grid)
+
+	for {
+		y = y + dy
+		x = x + dx
+
+		// Still valid?
+		if !(x >= 0 && x <= width-1 && y >= 0 && y <= height-1) {
+			return false
+		}
+
+		// Check value at position
+		if grid[y][x] == '.' {
+			continue
+		}
+		if grid[y][x] == 'L' {
+			return false
+		}
+		if grid[y][x] == '#' {
+			return true
+		}
+	}
 }
 
 func computeOccupiedNeighbourSeats(grid [][]byte, y int, x int) int {
