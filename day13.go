93fb8ffc46e07013c91366c933e63245645bd365
Michael Lesniak <mail@mlesniak.com>
2020-12-13 09:56:15 +0100

    Day 13 / Part 2 / Testcase

--- day13.go
+++ day13.go
@@ -8,28 +8,35 @@ import (
 func day13() {
 	timeplan := readLines("input/13.txt")
 
-	startTime, _ := strconv.Atoi(timeplan[0])
 	var busTimes []int
 	for _, busTime := range strings.Split(timeplan[1], ",") {
 		if busTime == "x" {
+			busTimes = append(busTimes, -1)
 			continue
 		}
 		bt, _ := strconv.Atoi(busTime)
 		busTimes = append(busTimes, bt)
 	}
 
-	var solution int
-	t := startTime
-loop:
-	for {
-		for _, bt := range busTimes {
-			if t%bt == 0 {
-				solution = (t - startTime) * bt
-				break loop
+	for t := 0; ; t++ {
+		found := true
+		for i := 0; i < len(busTimes); i++ {
+			if busTimes[i] == -1 {
+				// ignore, since 'x'
+				continue
+			}
+
+			okTime := t + i
+			if okTime%busTimes[i] != 0 {
+				// Not viable
+				found = false
+				break
 			}
 		}
-		t++
-	}
 
-	println(solution)
+		if found {
+			println(t)
+			break
+		}
+	}
 }
