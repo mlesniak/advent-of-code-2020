284bd42a71547ded9068318dbca98e897bee1782
Michael Lesniak <mail@mlesniak.com>
2020-12-14 06:28:53 +0100

    Day 14 / Part 1 / Testcase

--- day14.go
+++ day14.go
@@ -6,16 +6,20 @@ import (
 	"strconv"
 )
 
+var (
+	maskRegex = regexp.MustCompile(`mask = (.*)`)
+	memRegex  = regexp.MustCompile(`mem\[(\d)+\] = (\d+)`)
+)
+
 func day14() {
 	commands := readLines("input/14.txt")
 	memory := make(map[int]int)
 
-	maskRegex := regexp.MustCompile(`mask = (.*)`)
-	memRegex := regexp.MustCompile(`mem\[(\d)+\] = (\d+)`)
+	mask := ""
 	for _, command := range commands {
 		m1 := maskRegex.FindStringSubmatch(command)
 		if len(m1) > 1 {
-			mask := m1[1]
+			mask = m1[1]
 			fmt.Printf("%v\n", mask)
 			continue
 		}
@@ -24,6 +28,19 @@ func day14() {
 		if len(m2) > 1 {
 			dst, _ := strconv.Atoi(m2[1])
 			val, _ := strconv.Atoi(m2[2])
+
+			// Convert value.
+			//fmt.Printf("from value %v\n", val)
+			for i := len(mask) - 1; i >= 0; i-- {
+				if mask[i] == '1' {
+					val = setBit(val, len(mask)-i-1)
+				} else if mask[i] == '0' {
+					val = clearBit(val, len(mask)-i-1)
+				}
+			}
+			//fmt.Printf("masked value %v\n", val)
+
+			memory[dst] = val
 			fmt.Printf("%v <- %v\n", dst, val)
 			continue
 		}
@@ -37,4 +54,15 @@ func day14() {
 		sum += v
 	}
 	println(sum)
+}
+
+func setBit(n int, pos int) int {
+	n |= 1 << pos
+	return n
+}
+
+func clearBit(n int, pos int) int {
+	mask := ^(1 << pos)
+	n &= mask
+	return n
 }
