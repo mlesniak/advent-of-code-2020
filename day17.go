62500373411387b7801aeb7c6765be78b569b45c
Michael Lesniak <mail@mlesniak.com>
2020-12-18 06:28:51 +0100

    Day 17 / Testcase

--- day17.go
+++ day17.go
@@ -35,5 +35,68 @@ func day17() {
 		}
 	}
 
-	println(grid.String())
+	widthX := len(initialCells.Data[0])
+	widthY := len(initialCells.Data)
+	widthZ := 1
+
+	for cycle := 0; cycle < 6; cycle++ {
+		// Compute a single cycle
+		tmp := make(Grid3D)
+		for x := -widthX; x <= widthX; x++ {
+			for y := -widthY; y <= widthY; y++ {
+				for z := -widthZ; z <= widthZ; z++ {
+					// Compute number of neighbours
+					activeNeighbours := 0
+					for dx := -1; dx <= 1; dx++ {
+						for dy := -1; dy <= 1; dy++ {
+							for dz := -1; dz <= 1; dz++ {
+								if dx == 0 && dy == 0 && dz == 0 {
+									continue
+								}
+								x2 := x + dx
+								y2 := y + dy
+								z2 := z + dz
+								if grid[coordinate{x2, y2, z2}] {
+									activeNeighbours++
+								}
+							}
+						}
+					}
+
+					fmt.Printf("%v %v %v / %v -> %d\n", x, y, z,
+						grid[coordinate{x, y, z}],
+						activeNeighbours)
+					if grid[coordinate{
+						x: x,
+						y: y,
+						z: z,
+					}] && (activeNeighbours == 2 || activeNeighbours == 3) {
+						tmp[coordinate{
+							x: x,
+							y: y,
+							z: z,
+						}] = true
+					}
+					if !grid[coordinate{
+						x: x,
+						y: y,
+						z: z,
+					}] && activeNeighbours == 3 {
+						tmp[coordinate{
+							x: x,
+							y: y,
+							z: z,
+						}] = true
+					}
+				}
+			}
+		}
+
+		widthX += 1
+		widthY += 1
+		widthZ += 1
+		grid = tmp
+	}
+
+	println(len(grid))
 }
