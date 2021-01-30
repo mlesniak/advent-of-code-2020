9298ab961f27fb67562ccddc60c2f4a53a479360
Michael Lesniak <mail@mlesniak.com>
2021-01-30 18:45:34 +0100

    [20] WIP Reset

--- day20.go
+++ day20.go
@@ -28,36 +28,17 @@ func (t *tile) String() string {
 	return sb.String()
 }
 
-// 2971     1489     1171
-// 2729              2473
-//
-
-// 1951    2311
-// 2729
-
-// 2971    1489
-// 2729
-
-// 2311    3079
-//         2473
-
-// 1951    2311    3079
-// 2729    1427    2473
-// 2971    1489    1171
-
 func day20() {
 	tiles := readTiles()
 
 	for tileIndex := range tiles {
 		tile := &tiles[tileIndex]
-		//fmt.Printf("- TILE %v----------------------------------------------\n", tile.id)
 
 		for candIndex := range tiles {
 			if candIndex == tileIndex {
 				continue
 			}
 			candidate := &tiles[candIndex]
-			//fmt.Printf("? %v\n", candidate.id)
 
 		nextSide:
 			for candSide := 0; candSide <= 3; candSide++ {
@@ -69,36 +50,24 @@ func day20() {
 				for flip := 0; flip <= 1; flip++ {
 					for rotate := 0; rotate <= 3; rotate++ {
 						if matchTile(candidate, tile, candSide) {
-							fmt.Printf("%v %v %v\n", candidate.id, tile.id, candSide)
-							if candidate.id == 1759 && tile.id == 1193 {
-								fmt.Printf("1759\n%s\n", candidate.grid.String())
-								fmt.Printf("1191\n%s\n", tile.grid.String())
-							}
-							//fmt.Printf("! match candSide=%v to tileSide=%v\n", candSide, (candSide+2)%4)
-							//fmt.Printf("tile=\n%v\ncand=%v\n", tile, candidate)
 							linkTile(tile, candidate, (candSide+2)%4)
 							continue nextSide
 						}
-
-						//fmt.Printf("R90\n")
 						tile.rotate()
 					}
-					//fmt.Printf("FH\n")
 					tile.flip()
 				}
 			}
 		}
-
-		//fmt.Printf("*** %v\n", tile.id)
-		//for i := 0; i <= 3; i++ {
-		//	id := "</>"
-		//	if tile.sides[i] != nil {
-		//		id = fmt.Sprintf("%d", tile.sides[i].id)
-		//	}
-		//	fmt.Printf("%v -> %v\n", i, id)
-		//}
 	}
 
+	//analysis(tiles)
+
+	corners := computeProduct(tiles)
+	println(corners)
+}
+
+func analysis(tiles []tile) {
 	println(len(tiles))
 
 	howManyPointToMe := make(map[int]int)
@@ -119,143 +88,6 @@ func day20() {
 	println()
 	for k, v := range perSide {
 		fmt.Printf("sides=%d <- #%d\n", k, v)
-	}
-
-	corners := computeProduct(tiles)
-
-	// Rotate corners.
-	//for !(corners[0].sides[0] != nil && corners[0].sides[1] != nil) {
-	//	corners[0].rotate()
-	//}
-
-	//for !(corners[1].sides[1] != nil && corners[1].sides[2] != nil) {
-	//	corners[1].rotate()
-	//}
-
-	//for !(corners[2].sides[2] != nil && corners[2].sides[3] != nil) {
-	//	corners[2].rotate()
-	//}
-	//
-	//for !(corners[3].sides[3] != nil && corners[3].sides[0] != nil) {
-	//	corners[3].rotate()
-	//}
-	//
-	//for _, corner := range corners {
-	//	fmt.Printf("%v\n", corner.String())
-	//}
-
-	//head := &corners[1]
-	//for head.sides[2] != nil {
-	//
-	//	head = head.sides[2]
-	//}
-
-	// Top left corner
-	//direction := 1
-	//direction2 := 2
-	//rowStart := &corners[direction]
-	//
-	//for {
-	//	//Start with the corner and use opposites till end of row
-	//	cur := rowStart
-	//	for {
-	//		fmt.Printf("%v\n", cur.id)
-	//		next := cur.sides[direction]
-	//		if cur.sides[direction] == nil {
-	//			break
-	//		}
-	//
-	//		for i := 0; i <= 3; i++ {
-	//			if next.sides[i] != nil && cur.id == next.sides[i].id {
-	//				direction = (i + 2) % 4
-	//			}
-	//		}
-	//		cur = next
-	//	}
-	//
-	//	rowStart = rowStart.sides[direction2]
-	//	if rowStart == nil {
-	//		break
-	//	}
-	//}
-
-	seen := make(map[int]struct{})
-
-	start := &corners[0]
-	var dir1 int
-	var dir2 int
-	first := false
-	for i := 0; i <= 3; i++ {
-		if start.sides[i] != nil {
-			if !first {
-				dir1 = i
-				first = true
-				continue
-			} else {
-				dir2 = i
-				break
-			}
-		}
-	}
-
-	seen[start.id] = struct{}{}
-	println(dir1)
-	println(dir2)
-
-	print := false
-
-	// correct all rows
-	for {
-		// Correct a single row
-		head := start
-		for head != nil {
-			if head.id == 1471 {
-				print = true
-			}
-			fmt.Printf("%v ", head.id)
-			if print {
-				fmt.Printf("%s\n", head.String())
-				fmt.Printf("%s\n", head.grid.String())
-			}
-			next := head.sides[dir1]
-			if next == nil {
-				break
-			}
-
-			// solange rotieren, bis next.side[dir+2%4] == head.id ist
-			for {
-				if next.sides[(dir1+2)%4] != nil && next.sides[(dir1+2)%4].id == head.id {
-					break
-				}
-				//if next.sides[(dir1+2)%4] == nil {
-				//	next.rotate()
-				//	continue
-				//}
-
-				next.rotate() // maybe randomize to simplify?
-			}
-			head = next
-		}
-
-		println()
-		next := start.sides[dir2]
-		if next == nil {
-			break
-		}
-		for {
-			next.rotate() // maybe randomize to simplify?
-			if next.sides[(dir1+2)%4] == nil {
-				continue
-			}
-			if next.sides[(dir1+2)%4].id == start.id {
-				break
-			}
-		}
-		start = next
-
-		if print {
-			return
-		}
 	}
 }
 
@@ -289,18 +121,16 @@ func computeProduct(tiles []tile) []tile {
 				count++
 			}
 		}
-		//fmt.Printf("%v = %v\n", tile.id, count)
 		if count == 2 {
-			fmt.Printf("*** %v\n", tile.id)
-			for i := 0; i <= 3; i++ {
-				id := "</>"
-				if tile.sides[i] != nil {
-					id = fmt.Sprintf("%d", tile.sides[i].id)
-				}
-				fmt.Printf("%v -> %v\n", i, id)
-			}
+			//fmt.Printf("*** %v\n", tile.id)
+			//for i := 0; i <= 3; i++ {
+			//	id := "</>"
+			//	if tile.sides[i] != nil {
+			//		id = fmt.Sprintf("%d", tile.sides[i].id)
+			//	}
+			//	fmt.Printf("%v -> %v\n", i, id)
+			//}
 			prod *= int64(tile.id)
-
 			cornerTiles = append(cornerTiles, tile)
 		}
 	}
