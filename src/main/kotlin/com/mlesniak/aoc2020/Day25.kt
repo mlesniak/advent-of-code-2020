5114890cf96f8103d60988437b766d27cf36506f
Michael Lesniak <mail@mlesniak.com>
2021-02-21 11:29:02 +0100

    [25] Part 1

--- src/main/kotlin/com/mlesniak/aoc2020/Day25.kt
+++ src/main/kotlin/com/mlesniak/aoc2020/Day25.kt
@@ -2,12 +2,16 @@ package com.mlesniak.aoc2020
 
 class Day25 {
     fun main() {
-        val doorPublicKey = 5764801L
-        val doorLoop = findLoop(doorPublicKey)
+        // 12578151
+        // 5051300
+        // val doorPublicKey = 5764801L
+        val doorPublicKey = 12578151L
+        val doorLoop = findTransform(7, doorPublicKey)
         println(doorLoop)
 
-        val cardPublicKey = 17807724L
-        val cardLoop = findLoop(cardPublicKey)
+        // val cardPublicKey = 17807724L
+        val cardPublicKey = 5051300L
+        val cardLoop = findTransform(7, cardPublicKey)
         println(cardLoop)
 
         val encryptionKey = transform(doorPublicKey, cardLoop)
@@ -16,8 +20,8 @@ class Day25 {
         // println(t)
     }
 
-    private fun findLoop(doorPublicKey: Long): Int {
-        var size = 1
+    private fun findLoop(doorPublicKey: Long): Long {
+        var size = 1L
         while (true) {
             if (transform(7, size) == doorPublicKey) {
                 return size
@@ -26,11 +30,24 @@ class Day25 {
         }
     }
 
-    fun transform(subject: Long, loopSize: Int): Long {
+    fun transform(subject: Long, loopSize: Long): Long {
         var value = 1L
         for (i in 1..loopSize) {
             value *= subject
             value %= 20201227
+        }
+
+        return value
+    }
+
+    fun findTransform(subject: Long,target: Long): Long {
+        var value = 1L
+        for (i in 1..Long.MAX_VALUE) {
+            value *= subject
+            value %= 20201227
+            if (target == value) {
+                return i
+            }
         }
 
         return value
